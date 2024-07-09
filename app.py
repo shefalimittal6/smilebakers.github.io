700af04b8e5a4bf3c3b35c16f8b42b62e4cfc24a
shefalimittal6 <mittal.shefali2000@gmail.com>
2024-07-09 14:11:17 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, jsonify, request
+
+app = Flask(__name__)
+
+# Sample product data
+products = [
+    {"id": 1, "name": "Chocolate Cake", "price": "₹600", "image": "images/chocolate_cake.jpeg"},
+    {"id": 2, "name": "Vanilla Cake", "price": "₹500", "image": "images/vanilla_cake.jpg"},
+    {"id": 3, "name": "Strawberry Cake", "price": "₹550", "image": "images/strawberry_cake.jpg"},
+]
+
+@app.route('/')
+def index():
+    return render_template('index.html', products=products)
+
+@app.route('/search')
+def search():
+    query = request.args.get('query', '').lower()
+    filtered_products = [product for product in products if query in product['name'].lower()]
+    return render_template('index.html', products=filtered_products)
+
+if __name__ == '__main__':
+    app.run(debug=True)
