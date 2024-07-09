7242a361dc8d9099aa4a5ef3c77404701fa60620
shefalimittal6 <mittal.shefali2000@gmail.com>
2024-07-09 14:22:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,23 +1,17 @@
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 # Sample product data
 products = [
-    {"id": 1, "name": "Chocolate Cake", "price": "₹600", "image": "chocolate_cake.jpeg"},
-    {"id": 2, "name": "Vanilla Cake", "price": "₹500", "image": "vanilla_cake.jpg"},
-    {"id": 3, "name": "Strawberry Cake", "price": "₹550", "image": "strawberry_cake.jpg"},
+    {"id": 1, "name": "Chocolate Cake", "price": "$20", "image": "chocolate_cake.jpeg"},
+    {"id": 2, "name": "Vanilla Cake", "price": "$15", "image": "vanilla_cake.jpg"},
+    {"id": 3, "name": "Strawberry Cake", "price": "$25", "image": "strawberry_cake.jpg"},
 ]
 
 @app.route('/')
 def index():
     return render_template('index.html', products=products)
-
-@app.route('/search')
-def search():
-    query = request.args.get('query', '').lower()
-    filtered_products = [product for product in products if query in product['name'].lower()]
-    return render_template('index.html', products=filtered_products)
 
 if __name__ == '__main__':
     app.run(debug=True)
